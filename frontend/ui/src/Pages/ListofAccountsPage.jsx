3a55ef7e06dcf4b3652292486e0a4d97152c4026
MasbiMpho <mpholin15@gmail.com>
2026-05-14 23:50:32 +0200

    Added api connection functionality to list account

--- frontend/ui/src/Pages/ListofAccountsPage.jsx
+++ frontend/ui/src/Pages/ListofAccountsPage.jsx
@@ -1,7 +1,50 @@
+import { use, useEffect, useState } from "react";
+
+
 export default function ListofAccountsPage() {
+
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/users")
+      .then((response) => {
+        if(!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+        console.log("API response:", data); 
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
   return (
     <div>
         <h1>List of Accounts</h1>
+        <ul>
+            {users.map((user) => (
+                <li key={user.id}>
+                    Name: {user.userName} | Account Number: {user.accountNumber} | Deposit: R{user.balance.toFixed(2)} | Account Type: {user.accountType} | Status: {user.status}
+                </li>
+            ))}
+        </ul>
     </div>
   );
 }   
